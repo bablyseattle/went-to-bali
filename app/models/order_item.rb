e2ab19d335d70004a22aaf1c0ff07f4d8def614c
Bably Kumari <bably@Bablys-MacBook-Pro.local>
2017-12-18 02:59:24 -0800

    add validation to reject adding -ve quantity

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,5 +2,5 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
 
-  validates :quantity, presence: true
+  validates :quantity, numericality: { greater_than_or_equal_to: 1 }, presence: true
 end
