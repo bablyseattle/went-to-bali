3bfe5fa84826601ee5ed20b259ff1a6a0f52395c
Bably Kumari <bably@Bablys-MacBook-Pro.local>
2017-12-17 19:17:31 -0800

    add flash notice for user signup and sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,15 +7,18 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
+      flash[:notice] = "Welcome to Flipcart: Signed in sucessfully"
       redirect_to '/'
     else
+      flash[:notice] = "Something went wrong: Try again"
       redirect_to '/sign_in'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/sign_in'
+    flash[:notice] = "Logged out sucessfully"
+    redirect_to '/'
   end
 
 end
